58e6e8ff80121483844e2d32bc7b6dbfcdfe832d
Daniel Donaldson <ColorTurtle@Daniels-MacBook-Air.local>
2014-01-21 19:46:17 -0500

    Added 4 examples, 4th one not working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,3 +13,38 @@ $('.submit').click(function (){
 		}
 	});
 });
+
+// Try 1
+$('.try1').click(function(){
+	console.log('You can do this!');
+
+	$(this).addClass('active');
+
+});
+
+// Try 2
+$('.try2').hover(function(){
+	console.log('You can do this!');
+
+	$(this).addClass('active');
+
+});
+
+// Try 3
+$('.try3').click(function(){
+	console.log('You can do this!');
+
+	$(this).addClass('expand');
+
+});
+
+// Try 4
+$('.submit').focused(function (){
+	console.log('focused on an input');
+
+	$('input').each(function(){
+		if($(this).val() == '') {
+			$('.submit').addClass('its-off');
+		}
+	});
+});
