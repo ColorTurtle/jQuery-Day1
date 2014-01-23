36afc37afddc2f8ccc0d8575d6dbd3dc61ff1912
Daniel Donaldson <ColorTurtle@Daniels-MacBook-Air.local>
2014-01-23 00:20:45 -0500

    Finished Zombie javascript story

--- app/scripts/other.js
+++ app/scripts/other.js
@@ -26,10 +26,13 @@ var dog = {
 }
 
 var zombieFred = {
+	name: 'Fred',
 	legs:2,
 	arms:1,
 	alive: false,
+	scare: false,
 	headless: false,
+	interests: ["brains", "eating out", "long walks"],
 
 	groan: function(){
 		console.warn('Brains! Need to nom on brains!');
@@ -43,14 +46,20 @@ var zombieFred = {
 	scare:function(aVic){
 		aVic.scare=true
 		return "Ahhhhhhh!"
+	},
+
+	share:function(){
+		console.log('Hi, my name is ' +name+ ' and I love' +interests+ '. Gimme a holla.');
 	}
 }
 
 var Blondie = {
+	name: 'Blondie',
 	legs:2,
 	arms:2,
 	alive: true,
 	scare: false,
+	interests: ["physics", "staying indoors", "barfing"],
 	nordicKill:function(aVic){
 		aVic.headless=true
 		return "F**k you zed head!"
@@ -63,6 +72,7 @@ var Mason = {
 	arms: 2,
 	alive: true,
 	scare: false,
+	interests: ["Nordic Quests", "being a badass", "machete throwing"],
 
 	nordicKill:function(aVic){
 		aVic.headless=true
