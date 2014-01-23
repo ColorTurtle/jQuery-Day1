29a505c67fafe1469530c85e7ece0ebc26bc2376
Daniel Donaldson <ColorTurtle@Daniels-MacBook-Air.local>
2014-01-22 23:57:54 -0500

    Added Zombie story to the javascript

--- app/scripts/other.js
+++ app/scripts/other.js
@@ -24,3 +24,50 @@ var dog = {
 		console.log('I have ' +this.ears+ ' ears.');
 	}
 }
+
+var zombieFred = {
+	legs:2,
+	arms:1,
+	alive: false,
+	headless: false,
+
+	groan: function(){
+		console.warn('Brains! Need to nom on brains!');
+	},
+
+	kill:function(aVic){
+		aVic.alive=false
+		return "I'm still hungry for brains!"
+	},
+
+	scare:function(aVic){
+		aVic.scare=true
+		return "Ahhhhhhh!"
+	}
+}
+
+var Blondie = {
+	legs:2,
+	arms:2,
+	alive: true,
+	scare: false,
+	nordicKill:function(aVic){
+		aVic.headless=true
+		return "F**k you zed head!"
+	}
+
+}
+
+var Mason = {
+	legs:2,
+	arms: 2,
+	alive: true,
+	scare: false,
+
+	nordicKill:function(aVic){
+		aVic.headless=true
+		return "F**k you zed head!"
+	}
+}
+
+
